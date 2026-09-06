7454a6e554e036180c1081f00c1ef143e6880a1f
Yevhenii Lytvynenko <114651264+Yevhenii2022@users.noreply.github.com>
2026-09-06 15:50:05 +0300

    add forms

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -6,12 +6,14 @@ use App\Filament\Resources\CategoryResource\Pages;
 use App\Filament\Resources\CategoryResource\RelationManagers;
 use App\Models\Category;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\HtmlString;
 
 class CategoryResource extends Resource
 {
@@ -27,8 +29,28 @@ class CategoryResource extends Resource
     {
         return $form
             ->schema([
-                //
-            ]);
+                TextInput::make('title')
+                ->default('New Category')     
+                ->helperText(new HtmlString('Enter the name of the <strong>category</strong>'))
+                ->hint('Hint for title')
+                ->hintIcon('heroicon-m-language', 'Tooltip for title')
+                ->hintColor('info')
+                ->disabledOn('edit')
+                ->hiddenOn('edit')
+                ->autofocus()    
+                ->columnSpan(2)
+                ->label('Category Name')
+                ->required()
+                ->maxLength(255),             
+                TextInput::make('slug'),   
+                Forms\Components\Select::make('status')->options([
+                    1 => 'Draft', 'Published', 'Reviewing'
+                ]),          
+                TextInput::make('email')->email(),   
+                TextInput::make('password')->password()->revealable(),
+                TextInput::make('phone')->tel()->placeholder('xxx xxx-xx-xx')->mask('999 999-99-99'),
+                TextInput::make('domain')->prefix('https://')->suffix('.com')->suffixIcon('heroicon-m-globe-alt'),        
+            ])->columns(2);
     }
 
     public static function table(Table $table): Table
