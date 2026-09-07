f9f98fd32d4489a6440665044396dfae312b86b6
Yevhenii Lytvynenko <114651264+Yevhenii2022@users.noreply.github.com>
2026-09-07 14:34:04 +0300

    add form fields

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -6,12 +6,19 @@ use App\Filament\Resources\CategoryResource\Pages;
 use App\Filament\Resources\CategoryResource\RelationManagers;
 use App\Models\Category;
 use Filament\Forms;
+use Filament\Forms\Components\Builder;
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\KeyValue;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
+// use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\HtmlString;
 
@@ -43,9 +50,82 @@ class CategoryResource extends Resource
                 ->required()
                 ->maxLength(255),             
                 TextInput::make('slug'),   
+                FileUpload::make('image')
+                    // ->disk('public_uploads')
+                    ->directory("preview/" . date('Y') . '/' . date('m') . '/' . date('d'))
+                    ->imageEditor()
+                    ->reorderable()
+                    ->acceptedFileTypes(['image/png', 'image/jpeg'])
+                    ->imageEditorAspectRatios([
+                        null,
+                        '16:9',
+                        '4:3',
+                        '1:1',
+                    ])
+                    ->multiple()
+                    ->columnSpan(2),
                 Forms\Components\Select::make('status')->options([
                     1 => 'Draft', 'Published', 'Reviewing'
-                ]),          
+                ]),    
+                RichEditor::make('content')->columnSpan(2), 
+                
+                Repeater::make('users')
+                    ->schema([
+                        TextInput::make('name')->required()->live(true),
+                        Select::make('role')
+                            ->options([
+                                'user' => 'User',
+                                'manager' => 'Manager',
+                                'admin' => 'Admin',
+                            ])
+                            ->required(),
+                    ])
+                    ->addActionLabel('Add user')
+                    ->cloneable()
+                    ->collapsible()
+                    ->itemLabel(fn (array $state): ?string => $state['name'] ?? null)
+//                    ->defaultItems(3)
+                    ->columns(2)->columnSpan(2),
+
+                KeyValue::make('meta')->columnSpan(2),
+
+                Forms\Components\Builder::make('page_content')
+                    ->blocks([
+                        Block::make('heading')
+                            ->schema([
+                                TextInput::make('content')
+                                    ->label('Heading')
+                                    ->required(),
+                                Select::make('level')
+                                    ->options([
+                                        'h1' => 'Heading 1',
+                                        'h2' => 'Heading 2',
+                                        'h3' => 'Heading 3',
+                                        'h4' => 'Heading 4',
+                                        'h5' => 'Heading 5',
+                                        'h6' => 'Heading 6',
+                                    ])
+                                    ->required(),
+                            ])
+                            ->columns(2),
+                        Block::make('paragraph')
+                            ->schema([
+                                Forms\Components\Textarea::make('content')
+                                    ->label('Paragraph')
+                                    ->required(),
+                            ]),
+                        Block::make('image')
+                            ->schema([
+                                Forms\Components\FileUpload::make('url')
+                                    ->label('Image')
+                                    ->image()
+                                    ->required(),
+                                TextInput::make('alt')
+                                    ->label('Alt text')
+                                    ->required(),
+                            ]),
+                    ])->columnSpan(2),
+
                 TextInput::make('email')->email(),   
                 TextInput::make('password')->password()->revealable(),
                 TextInput::make('phone')->tel()->placeholder('xxx xxx-xx-xx')->mask('999 999-99-99'),
