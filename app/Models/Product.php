4bbd45662523905e89332f49b0c84b65a869ec96
Yevhenii Lytvynenko <114651264+Yevhenii2022@users.noreply.github.com>
2026-09-12 17:50:58 +0300

    add Filter

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -11,4 +12,9 @@ class Product extends Model
     protected $casts = [
         'photos' => 'array',
     ];
+
+    public function filters(): BelongsToMany
+    {
+        return $this->belongsToMany(Filter::class, 'filter_product', 'product_id', 'filter_id');
+    }
 }
